1b67c1901e1a0ba0f295e835dcfc087236115b17
Nika Golestani <ng8224@gmail.com>
2023-02-13 01:44:03 +0300

    handle 429 axios err

--- src/Today.js
+++ src/Today.js
@@ -13,53 +13,77 @@ export default function Today(props) {
     axios.get(apiUrl).then(handleResponse);
   }
   function handleResponse(response) {
-    setWeatherData({
-      ready: true,
-      coordinates: response.data.coord,
-      temperature: response.data.temperature.current,
-      humidity: response.data.temperature.humidity,
-      date: new Date(response.data.time * 1000),
-      description: response.data.condition.description,
-      icon: response.data.condition.icon,
-      wind: response.data.wind.speed,
-      city: response.data.city,
-      oldProps: props.city,
-    });
+    if (response.data.status !== "not_found") {
+      setWeatherData({
+        ready: true,
+        valid: true,
+        coordinates: response.data.coord,
+        temperature: response.data.temperature.current,
+        humidity: response.data.temperature.humidity,
+        date: new Date(response.data.time * 1000),
+        description: response.data.condition.description,
+        icon: response.data.condition.icon,
+        wind: response.data.wind.speed,
+        city: response.data.city,
+        oldProps: props.city,
+      });
+    } else {
+      setWeatherData({
+        ready: true,
+        valid: false,
+        msg: response.data.message,
+        oldProps: props.city,
+      });
+    }
   }
   if (weatherData.ready) {
-    return (
-      <div className="todaydiv">
-        <div className="today">
-          <h1 className="p-0 m-0" id="city">
-            {weatherData.city}
-          </h1>
-          <h4 className="p-0 m-0 text-secondary " id="date">
-            <DateElement date={weatherData.date} />
-          </h4>
-          <h4 className="p-0 text-secondary wvalue">
-            {weatherData.description}
-          </h4>
-          <div className="degreec">
-            <span className="num">
-              <span className="int">{Math.round(weatherData.temperature)}</span>
-              °
+    if (weatherData.valid) {
+      return (
+        <div className="todaydiv">
+          <div className="today">
+            <h1 className="p-0 m-0" id="city">
+              {weatherData.city}
+            </h1>
+            <h4 className="p-0 m-0 text-secondary " id="date">
+              <DateElement date={weatherData.date} />
+            </h4>
+            <h4 className="p-0 text-secondary wvalue">
+              {weatherData.description}
+            </h4>
+            <div className="degreec">
+              <span className="num">
+                <span className="int">
+                  {Math.round(weatherData.temperature)}
+                </span>
+                °
+              </span>
+              <span className=" d-block text-secondary text-center self-align-end p-2"></span>
+            </div>
+            <span id="emoji">
+              <Emoji code={weatherData.icon} />
             </span>
             <span className=" d-block text-secondary text-center self-align-end p-2"></span>
           </div>
-          <span id="emoji">
-            <Emoji code={weatherData.icon} />
-          </span>
-          <span className=" d-block text-secondary text-center self-align-end p-2"></span>
-        </div>
-        <br />
-        <div className="text-secondary today details">
-          Wind: {weatherData.wind} km/h
-          <Emoji code="wind-day" size={20} />
           <br />
-          Humidity: {weatherData.humidity}%
+          <div className="text-secondary today details">
+            Wind: {weatherData.wind} km/h
+            <Emoji code="wind-day" size={20} />
+            <br />
+            Humidity: {weatherData.humidity}%
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="todaydiv">
+          <div className="today">{weatherData.msg}</div>
+          <br />
+          <div className="text-secondary today details">
+            <p>{weatherData.msg}</p>
+          </div>
+        </div>
+      );
+    }
   } else {
     return (
       <div className="todaydiv">
